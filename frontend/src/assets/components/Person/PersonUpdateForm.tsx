9ca2c12d7d1d15178c5cf84084d8fcc203a190b3
Romariok <ronya-ko@yandex.ru>
2024-11-24 17:42:16 +0300

    some structure changes

---
+++ frontend/src/assets/components/Person/PersonUpdateForm.tsx
@@ -0,0 +1,309 @@
+import React, { useEffect, useState } from 'react';
+import {
+   Box,
+   Input,
+   Modal,
+   Snackbar,
+   Alert,
+   Checkbox,
+   TextField,
+   MenuItem
+} from '@mui/material';
+import { appSelector, clearState, sendUpdatedPerson } from "../../../storage/Slices/AppSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { IUpdatePerson } from "../../../storage/Slices/AppSlice";
+import StyleButton from '../StyleButton';
+import { AppDispatch } from '../../../storage/store';
+import { Color, Nationality } from './PersonTable';
+
+
+interface CoordinateFormProps {
+   open: boolean;
+   onClose: () => void;
+}
+
+interface FormData {
+   id: number;
+   eyeColor: Color;
+   hairColor: Color | null;
+   name: string;
+   nationality: Nationality | null;
+   weight: string;
+   locationId: string;
+   adminCanModify: boolean;
+}
+
+const colors = [{ value: "GREEN", label: "Green" }, { value: "BLUE", label: "Blue" }, { value: "WHITE", label: "White" }, { value: "BROWN", label: "Brown" }];
+const colorsNULL = [{ value: "GREEN", label: "Green" }, { value: "BLUE", label: "Blue" }, { value: "WHITE", label: "White" }, { value: "BROWN", label: "Brown" }, { value: "", label: "NULL" }];
+const nationalities = [{ value: "RUSSIA", label: "RU" }, { value: "UNITED_KINGDOM", label: "UK" }, { value: "VATICAN", label: "VA" }, { value: "ITALY", label: "IT" }, { value: "THAILAND", label: "TH" }];
+
+const PersonUpdateForm: React.FC<CoordinateFormProps> = ({ open, onClose }) => {
+   const dispatch = useDispatch<AppDispatch>();
+   const { isFetching, isSuccess, isError, errorMessage, locations, person } = useSelector(appSelector);
+   const [openError, setOpenError] = useState<boolean>(false);
+   const [formData, setFormData] = useState<FormData>({
+      id: 0,
+      eyeColor: '' as Color,
+      hairColor: '' as Color,
+      name: '',
+      nationality: '' as Nationality,
+      weight: '',
+      locationId: '',
+      adminCanModify: false
+   });
+
+   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const newData = {
+         id: person?.id,
+         eyeColor: formData.eyeColor === '' ? null : formData.eyeColor,
+         hairColor: formData.hairColor === '' ? null : formData.hairColor,
+         name: formData.name === '' ? null : formData.name,
+         nationality: formData.nationality === '' ? null : formData.nationality,
+         weight: formData.weight === '' ? null : Number(formData.weight),
+         locationId: formData.locationId === '' ? null : Number(formData.locationId),
+         adminCanModify: formData.adminCanModify
+      };
+      dispatch(sendUpdatedPerson(newData as IUpdatePerson));
+   };
+
+   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      if (name === 'adminCanModify') {
+         setFormData((prevState) => ({
+            ...prevState,
+            [name]: e.target.checked,
+         }));
+      }
+      else {
+         setFormData((prevState) => ({
+            ...prevState,
+            [name]: value
+         }));
+      }
+   };
+
+   useEffect(() => {
+      formData.id = person === null ? 0 : person.id;
+      formData.eyeColor = person === null ? '' as Color : person.eyeColor;
+      formData.hairColor = person === null || person.hairColor === null ? '' as Color : person.hairColor;
+      formData.name = person === null ? '' : person.name;
+      formData.nationality = person === null ? '' as Nationality : person.nationality;
+      formData.weight = person === null ? '' : person.weight.toString();
+      formData.locationId = person === null ? '' : String(person.location.id);
+      formData.adminCanModify = person === null ? false : person.adminCanModify;
+
+      if (isError) {
+         console.log("Error: " + errorMessage);
+         setOpenError(true);
+         const timer = setTimeout(() => {
+            setOpenError(false);
+            dispatch(clearState());
+         }, 3000);
+         return () => clearTimeout(timer);
+      }
+
+      if (isSuccess) {
+         console.log("Person Succesfully Location");
+         onClose();
+         dispatch(clearState());
+      }
+   }, [isError, isSuccess, dispatch, person]);
+
+   return (
+      <>
+         <Modal
+            open={open}
+            onClose={onClose}
+            aria-labelledby="person-form-modal"
+            aria-describedby="person-form-description"
+         >
+            <Box
+               component="form"
+               noValidate
+               sx={{
+                  mt: 1,
+                  marginTop: '8',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  padding: '20px',
+                  textAlign: 'center',
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  width: 400,
+                  border: '2px solid #000',
+                  p: 4,
+                  borderColor: 'white',
+                  borderWidth: '6px',
+                  borderStyle: 'solid',
+               }}
+               onSubmit={handleFormSubmit}
+
+            >
+               <TextField
+                  margin="dense"
+                  type='text'
+                  select
+                  variant='standard'
+                  required
+                  fullWidth
+                  id="eyeColor"
+                  name="eyeColor"
+                  autoComplete="eyeColor"
+                  autoFocus
+                  label="Eye Color"
+                  defaultValue={""}
+                  value={formData.eyeColor}
+                  onChange={handleChange}
+                  color='primary'
+                  sx={{ color: 'white', mb: 1, WebkitTextFillColor: 'white' }}
+               >{colors.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                     {option.label}
+                  </MenuItem>
+               ))}</TextField>
+
+               <TextField
+                  margin="dense"
+                  type='text'
+                  select
+                  variant='standard'
+                  required
+                  fullWidth
+                  id="hairColor"
+                  name="hairColor"
+                  autoComplete="hairColor"
+                  autoFocus
+                  label="Hair Color"
+                  defaultValue={""}
+                  value={formData.hairColor}
+                  onChange={handleChange}
+                  color='primary'
+                  sx={{ color: 'white', mb: 1, WebkitTextFillColor: 'white' }}
+               >{colorsNULL.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                     {option.label}
+                  </MenuItem>
+               ))}</TextField>
+
+               <Input
+                  margin="dense"
+                  type='text'
+                  required
+                  fullWidth
+                  id="name"
+                  name="name"
+                  autoComplete="name"
+                  autoFocus
+                  value={formData.name}
+                  onChange={handleChange}
+                  sx={{ color: 'white', mb: 1 }}
+                  placeholder='Name (Not NULL)'
+               />
+
+               <TextField
+                  margin="dense"
+                  type='text'
+                  select
+                  variant='standard'
+                  required
+                  fullWidth
+                  id="nationality"
+                  name="nationality"
+                  autoComplete="nationality"
+                  autoFocus
+                  label="Nationality"
+                  defaultValue={""}
+                  value={formData.nationality}
+                  onChange={handleChange}
+                  color='primary'
+                  sx={{ color: 'white', mb: 1, WebkitTextFillColor: 'white' }}
+               >{nationalities.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                     {option.label}
+                  </MenuItem>
+               ))}</TextField>
+
+
+               <Input
+                  margin="dense"
+                  type='number'
+                  required
+                  fullWidth
+                  id="weight"
+                  name="weight"
+                  autoComplete="weight"
+                  autoFocus
+                  value={formData.weight}
+                  onChange={handleChange}
+                  sx={{ color: 'white', mb: 1 }}
+                  inputProps={{ min: 0 }}
+                  placeholder='Weight (Positive)'
+               />
+
+               <TextField
+                  margin="dense"
+                  type='text'
+                  select
+                  variant='standard'
+                  required
+                  fullWidth
+                  id="locationId"
+                  name="locationId"
+                  autoComplete="locationId"
+                  autoFocus
+                  label="Location ID"
+                  defaultValue={""}
+                  value={formData.locationId}
+                  onChange={handleChange}
+                  color='primary'
+                  sx={{ color: 'white', mb: 1, WebkitTextFillColor: 'white' }}
+               >{locations !== undefined ? locations.map((option) => (
+                  <MenuItem key={option.id} value={option.id}>
+                     {option.name}
+                  </MenuItem>)) : ''}
+
+               </TextField>
+
+               <Box sx={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center'
+               }}>
+                  <Checkbox
+                     required
+                     id="adminCanModify"
+                     name="adminCanModify"
+                     autoFocus
+                     checked={formData.adminCanModify}
+                     onChange={handleChange}
+                     sx={{ color: 'white', mb: 1 }}
+                  />
+                  <label style={{
+                     fontFamily: "Undertale",
+                     backgroundColor: 'black',
+                     color: 'white'
+                  }}>
+                     ADMIN CAN MODIFY
+                  </label>
+               </Box>
+
+
+               <StyleButton text="Create Person"
+                  disabled={isFetching}
+                  type="submit" />
+            </Box>
+         </Modal>
+         <Snackbar open={openError} autoHideDuration={3000} onClose={() => setOpenError(false)}>
+            <Alert severity="error" sx={{ fontFamily: "Undertale" }}>
+               {errorMessage}
+            </Alert>
+         </Snackbar>
+      </>
+   );
+};
+
+export default PersonUpdateForm;
---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,123 @@
+import {
+  Link,
+
+} from 'react-router-dom';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { appSelector, clearState, getAdminRequest, getCoordinates, getLocation, getMovie, getMovieAll, getPerson, getUserRole, setCoordinatesPage } from "../storage/Slices/AppSlice";
+
+import SockJS from "sockjs-client";
+import { Stomp } from "@stomp/stompjs";
+
+import { Grid, Paper } from '@mui/material';
+import { List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+
+import '/src/assets/css/main_page.css'
+
+import AddModeratorIcon from '@mui/icons-material/AddModerator';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt'; //Coordinate
+import AddLocationIcon from '@mui/icons-material/AddLocation'; // Location
+import MovieCreationIcon from '@mui/icons-material/MovieCreation';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import MapIcon from '@mui/icons-material/Map';
+import { AppDispatch } from '../storage/store';
+import React, { useEffect } from 'react';
+
+interface ListItemLinkProps {
+  icon?: React.ReactElement<unknown>;
+  primary: string;
+  to: string;
+}
+
+function ListItemLink(props: ListItemLinkProps) {
+  const { icon, primary, to } = props;
+
+  return (
+    <ListItemButton component={Link} to={to} onClick={switchPlay}>
+      {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+      <ListItemText primary={primary} />
+    </ListItemButton>
+  );
+}
+
+const switchTabs = new Audio('src/assets/sounds/switch_tabs.mp3');
+switchTabs.volume = 0.2;
+const switchPlay = () => {
+  switchTabs.play();
+}
+
+function MainPage() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { coordinatesPage, locationPage, personPage, moviePage, adminRequestPage } = useSelector(appSelector);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(clearState());
+      dispatch(getCoordinates(coordinatesPage));
+      dispatch(getLocation(locationPage));
+      dispatch(getPerson(personPage));
+      dispatch(getMovie(moviePage));
+      dispatch(getMovieAll());
+      dispatch(getAdminRequest(adminRequestPage));
+
+      if (localStorage.getItem('username') !== undefined && localStorage.getItem('username') !== null) {
+        dispatch(getUserRole(localStorage.getItem('username') as string));
+      }
+      const sock = new SockJS("http://localhost:5252/ws");
+      const stompClient = Stomp.over(sock);
+      stompClient.connect({
+        headers: {
+          "Authorization": `Bearer ${localStorage.getItem('token')}`,
+        }
+      }, () => {
+        stompClient.subscribe("/topic", (message) => {
+          dispatch(getCoordinates(coordinatesPage));
+          dispatch(getLocation(locationPage));
+          dispatch(getPerson(personPage));
+          dispatch(getMovie(moviePage));
+          dispatch(getMovieAll());
+          dispatch(getAdminRequest(adminRequestPage));
+        },
+          {
+            "Authorization": `Bearer ${localStorage.getItem('token')}`,
+          }
+        )
+      })
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+
+      <Paper elevation={0} sx={{ backgroundColor: 'black', pt: "20%" }}>
+        <List sx={{
+          color: 'white',
+          backgroundColor: 'black',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          scale: "200%",
+        }}>
+          <Grid container sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+            <Grid item sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+              <ListItemLink to="/app/movie" primary="Movie" icon={<MovieCreationIcon color='primary' />} />
+              <ListItemLink to="/app/location" primary="Location" icon={<AddLocationIcon color='primary' />} />
+              <ListItemLink to="/app/coordinates" primary="Coordinates" icon={<AddLocationAltIcon color='primary' />} />
+            </Grid>
+            <Grid item sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+              <ListItemLink to="/app/person" primary="Person" icon={<PersonAddIcon color='primary' />} />
+              <ListItemLink to="/app/admin" primary="Admin Requests" icon={<AddModeratorIcon color='primary' />} />
+              <ListItemLink to="/app/special" primary="Special Functions" icon={<MoreHorizIcon color='primary' />} />
+            </Grid>
+            <ListItemLink to="/app/map" primary="Map" icon={<MapIcon color='primary' />} />
+          </Grid>
+
+        </List>
+      </Paper>
+    </>
+  )
+}
+export default MainPage
